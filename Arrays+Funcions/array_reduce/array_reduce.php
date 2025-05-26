ed18af2a531cf7aa3b007fc2b573fb62bb333fe5
SharmarAndrey <sharmarandrey@gmail.com>
2025-05-27 01:58:08 +0200

    Ejercicios array_reduce

--- Arrays+Funcions/array_reduce/array_reduce.php
+++ Arrays+Funcions/array_reduce/array_reduce.php
@@ -21,3 +21,43 @@ echo $maxPrice;
 $productList = rtrim(array_reduce($products, fn ($carry, $product) => $carry . $product["tittle"] .  ", ", ""), ", ");
 echo "<br>";
 echo $productList;
+
+
+$users = [
+    ["name" => "Andrey", "age" => 42, "is_admin" => true, "gender" => "male"],
+    ["name" => "Olga", "age" => 30, "is_admin" => false, "gender" => "female"],
+    ["name" => "Ivan", "age" => 17, "is_admin" => false, "gender" => "male"],
+    ["name" => "Lena", "age" => 65, "is_admin" => true, "gender" => "female"],
+    ["name" => "Oleg", "age" => 50, "is_admin" => false, "gender" => "male"]
+];
+
+//General age of all users
+$summaAges = array_reduce($users, fn ($carry, $user) => $carry + $user["age"], 0);
+echo "<br>";
+echo $summaAges;
+
+//How many admins?
+$admins = array_reduce($users, fn ($carry, $user) => $carry + $user["is_admin"], 0);
+echo "<br>";
+echo $admins;
+
+//The average age of men
+
+$men = array_reduce($users, function ($carry, $user) {
+    if ($user["gender"] === "male") {
+        $carry["sum"] += $user["age"];
+        $carry["count"]++;
+    }
+    return $carry;
+}, ["sum" => 0, "count" => 0]);
+
+$averageAgeOfMen = $men["sum"] / $men["count"];
+echo "<br>";
+echo $averageAgeOfMen;
+
+
+//List of names only women in a line ("Olga, Lena"), without a comma at the end
+
+$women = rtrim(array_reduce($users, fn ($carry, $user) => $carry . ($user["gender"] == "female" ? $user["name"] . ", " : ""), ""), ", ");
+echo "<br>";
+echo $womans;
